693c79160d908df7fbff1dc537e5ace5b5620696
Tvisher <tvisher@mail.ru>
2022-09-13 15:29:01 +0600

    +

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div
+      v-show="!appLoad"
+      class="fixed w-100 h-100 bg-purple-800 inset-0 z-50 flex items-center justify-center"
+    >
+      <svg
+        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+
     <div class="container">
       <div class="w-full my-4"></div>
       <section>
@@ -12,6 +38,7 @@
               <input
                 v-model="ticker"
                 @keydown.enter="add"
+                @keydown="alreadyAdded = false"
                 @input="findChoices"
                 type="text"
                 name="wallet"
@@ -158,6 +185,7 @@ export default {
       graph: [],
       alreadyAdded: false,
       choices: [],
+      appLoad: false,
     };
   },
   created: function () {
@@ -166,6 +194,7 @@ export default {
       .then((data) => {
         const cryptoList = Object.keys(data.Data);
         localStorage.setItem("cryptonomicon-list", JSON.stringify(cryptoList));
+        this.appLoad = true;
       });
   },
 
@@ -219,15 +248,17 @@ export default {
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
     },
+
     findChoices() {
       const cryptoList = JSON.parse(localStorage.getItem("cryptonomicon-list"));
       if (!cryptoList) return;
-      let filteredList = cryptoList.filter((item) =>
-        item.startsWith(this.ticker.toUpperCase())
+      let filteredList = cryptoList.filter(
+        (item) => this.ticker && item.startsWith(this.ticker.toUpperCase())
       );
       filteredList = filteredList.slice(-4);
       this.choices = filteredList;
     },
+
     addChoise(choise) {
       this.ticker = choise;
       this.add();
